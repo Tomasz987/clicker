72be6a3a9a09bfb26322486adeda8b319290841a
Tomasz987 <tomek987125@gmail.com>
2022-11-19 20:46:09 +0100

    Typo in docstrings

--- keys_collection.py
+++ keys_collection.py
@@ -1,15 +1,15 @@
-"""Keys collection"""
+"""The keys collection"""
 from enum import Enum
 
 
 class Key(Enum):
-    """Keys for test"""
+    """The keys for test"""
     q = 0
     b = 1
 
 
 class Button(Enum):
-    """Buttons for test"""
+    """The buttons for test"""
     left = 0
     middle = 1
     right = 2
